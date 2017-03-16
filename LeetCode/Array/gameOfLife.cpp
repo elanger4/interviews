66d9817bde0bc01fc73d138ec6b3caf5b4756d2f
Erik Langert <eriklangert@gmail.com>
2017-03-16 00:35:49 -0400

    Works but with memory leaks.

--- LeetCode/Array/gameOfLife.cpp
+++ LeetCode/Array/gameOfLife.cpp
@@ -12,37 +12,83 @@
 // Could you solve it in-place? Remember that the board needs to be updated at the same time: You cannot update some cells first and then use their updated values to update other cells.
 // In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
 
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include "stdlib.h"
+
 int getNumNeighbors(int ** board, int i, int j,int n, int m) {
 
     int numNeighbors = 0;
-    for (int row = -1; i <= 1; ++i) {
-        for (int col = -1; j <= 1; ++j) {
-            if (i + row <= n-1 && i + row >= 0 && j + col <= m-1 && j + col >= 0) {
-               numNeighbors += 1; 
-            }
+    for (int row = std::max(i-1, 0); row <= std::min(i+1, n-1); ++row) {
+        for (int col = std::max(j-1, 0); col <= std::min(j+1, m-1); ++col) {
+            numNeighbors += board[row][col] & 1; 
         }
     }
-    return numNeighbors;
+
+    return numNeighbors - (board[i][j] & 1);
 }
 
 int ** nextState(int ** board, int n, int m) {
-    int ** newState = board;
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             int numNeighbors = getNumNeighbors(board, i, j, n, m);
-            if (numNeighbors > 3 || numNeighbors < 2) {
-                newState[i][j] = 0;
-            } else if (numNeighbors == 3) {
-                newState[i][j] = 1;
+            if (board[i][j] == 1 && numNeighbors <= 3 && numNeighbors >= 2) {
+                board[i][j] = 3;
+            } 
+            if (board[i][j] == 0 && numNeighbors == 3) {
+                board[i][j] = 2;
             }
-            // else numNeighbors == 2, alive cell will stay alive and dead cell
-            // will stay dead. No need to check for it.
         }
     }
-    return newState;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            board[i][j] >>= 1;
+        }
+    }
+
+    return board;
+}
+
+void printBoard(int ** board, int n, int m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            std::cout << board[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
 }
 
 int main() {
-    return 0;
+
+    int n = 20;
+    int m = 20;
+
+    int ** board = (int **)malloc(sizeof(int*) * n);
+    for (int i = 0; i < n; ++i) {
+        board[i] = (int*) malloc(sizeof(int) * m);
+    }
+
+    int i;
+    for (i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            board[i][j] = rand() % 2;
+        }
+    }
+
+    printBoard(board, n, m);
+    std::cout << std::endl;
+
+
+    i = 0;
+    while(i < 100) {
+        board = nextState(board, n, m);
+        printBoard(board, n, m);
+        ++i;
+        std::cout << std::endl;
+    }
+
+    free(board); 
 }
